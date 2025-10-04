9d3d0bcca9736bc9fac6d993c421c78112b46406
TecnoKB7j <emmanueladejuwon2021@gmail.com>
2025-10-04 07:34:34 +0100

    Add Firebase configuration file

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyD2tSH-ZKj6HorJCJIZ0q9hf_fY_nT5PVc",
+  authDomain: "fbi-vpn.firebaseapp.com",
+  projectId: "fbi-vpn",
+  storageBucket: "fbi-vpn.firebasestorage.app",
+  messagingSenderId: "724938549842",
+  appId: "1:724938549842:web:cc92681b2a615cf1fe0d8b",
+  measurementId: "G-31P4GRNEW5"
+};
